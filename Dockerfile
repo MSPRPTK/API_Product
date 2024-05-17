565e302112192fa90f4bab42f3eb4628083193c1
LemonBlanc <83752129+BWBMaxime@users.noreply.github.com>
2024-05-17 12:17:20 +0000

    Dockerfile et dockercompose fonctionnel

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM node:21.7.3
 
 # Définir le répertoire de travail dans le conteneur
-WORKDIR /usr/src/app
+WORKDIR /usr/src/app/API_Product
 
 # Copier le fichier package.json et package-lock.json
 COPY package*.json ./
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,11 +14,7 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://mongo:27017/votre_base_de_données', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connexion à MongoDB établie'))
-.catch(err => console.error('Erreur de connexion à MongoDB :', err));
+// Connexion à MongoDB
+mongoose.connect(process.env.MONGO_URL || 'mongodb://mongo:27017/db_product')
+  .then(() => console.log('Connexion à MongoDB établie'))
+  .catch(err => console.error('Erreur de connexion à MongoDB :', err));
