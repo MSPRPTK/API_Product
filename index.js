16586f4210de64675cf1ff1aff2024339c9421ad
LemonBlanc <83752129+BWBMaxime@users.noreply.github.com>
2024-05-17 13:27:03 +0000

    Test connection db

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const productRoutes = require('./routes/productRoute');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 
 
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use('/product', productRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/db_product')
+mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/db_product')
    .then(() => {
        console.log('Connected to MongoDB');
    }).catch((error) => {
